3c39a3dce25d2bce1660cedaf32671983141f62c
Robson <robson@coperve.ufsc.br>
2024-04-15 09:26:43 -0300

    Eventos

---
+++ resources/views/evento/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('content')
+    <main id="main">
+        <section id="services" class="services">
+            <div class="container" data-aos="">
+                <div class="section-title">
+                    <h2 class="title">Eventos</h2>
+                    <p>Listagem de Eventos</p>
+                </div>
+                <div class="row">
+                    
+                </div>
+            </div>
+        </section>
+    </main>
+@endsection

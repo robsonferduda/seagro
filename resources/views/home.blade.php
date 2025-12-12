c40f3821f035cb00fe8880a1363846c6ed38ff51
Robson <robson@coperve.ufsc.br>
2025-12-12 09:44:10 -0300

    Assembléia

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -57,6 +57,7 @@
         
         <div class="row" style="margin-bottom: 15px;">
           
+          <!--
           <div class="col-xs-12 col-sm-12 col-md-2 text-center mb-3">
             <a href="https://votacao.link/eleicao_seagro"> 
               <img src="{{ asset('img/banner/vote_aqui.jpeg') }}" style="min-height: 160px;" class="d-block w-100 img-radius" alt="Eleições SEAGRO-SC">
@@ -68,22 +69,24 @@
               <img src="{{ asset('img/banner/banner_eleicao.jpeg') }}" style="min-height: 160px;" class="d-block w-100 img-radius" alt="Eleições SEAGRO-SC">
             </a>
           </div>
-          <div class="col-xs-12 col-sm-12 col-md-2 text-center mb-3">
+          -->
+
+          <div class="col-xs-12 col-sm-12 col-md-3 text-center mb-3">
             <a href="https://www.calameo.com/read/0024823218cec57041ed2"> 
               <img src="{{ asset('img/banner/revista_40_anos.jpg') }}" style="min-height: 160px;" class="d-block w-100 img-radius" alt="Revista SEAGRO">
             </a>
           </div>
-          <div class="col-xs-12 col-sm-12 col-md-2  text-center mb-3">
+          <div class="col-xs-12 col-sm-12 col-md-3  text-center mb-3">
             <a href=" {{ url('destaque/boletim') }}">
               <img src="{{ asset('img/banner/boletim.jpg') }}" style="min-height: 160px;" class="d-block w-100 img-radius" alt="Boletins">
             </a>
           </div>
-          <div class="col-xs-12 col-sm-12 col-md-2  text-center mb-3">
+          <div class="col-xs-12 col-sm-12 col-md-3  text-center mb-3">
             <a href=" {{ url('destaque/linha_do_tempo') }}">
               <img src="{{ asset('img/banner/linha_do_tempo.jpg') }}" style="min-height: 160px;" class="d-block w-100 img-radius" alt="Linha do Tempo">
             </a>
           </div>
-          <div class="col-xs-12 col-sm-12 col-md-2  text-center mb-3">
+          <div class="col-xs-12 col-sm-12 col-md-3  text-center mb-3">
             <a href=" {{ url('destaque/contribuicao-sindical') }}">
               <img src="{{ asset('img/banner/contribuicao_sindical.jpeg') }}" style="min-height: 160px;" class="d-block w-100 img-radius" alt="Contribuição Sindical">
             </a>
